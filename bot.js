6dacfe7c08eab45c6f648d7fe2e6639327bbc7a3
donggoolosori <kdj6551@naver.com>
2021-03-23 16:03:16 +0900

    Get today's contribution count

---
+++ bot.js
@@ -0,0 +1,50 @@
+import telegram from 'node-telegram-bot-api';
+import cron from 'node-cron';
+import { gql, GraphQLClient } from 'graphql-request';
+import moment from 'moment';
+
+const bot = new telegram('1769867744:AAEqitqeJLy897CqSW_NH9eIl2wmW1yvFmY', {
+  polling: true,
+});
+const endpoint = 'https://api.github.com/graphql';
+
+const graphQLClient = new GraphQLClient(endpoint, {
+  headers: {
+    authorization: 'Token 8dc3ae6cf7244d5a36aeacb87da8f0c713c36f43',
+  },
+});
+
+cron.schedule('* * * * *', async () => {
+  const today = moment();
+  const from = today.startOf('day').format();
+  const to = today.endOf('day').format();
+
+  const query = gql`
+    query($user: String!, $from: DateTime, $to: DateTime) {
+      user(login: "donggoolosori") {
+        contributionsCollection(from: $from, to: $to) {
+          contributionCalendar {
+            totalContributions
+          }
+        }
+      }
+    }
+  `;
+  const variables = {
+    user: 'donggoolosori',
+    from: from,
+    to: to,
+  };
+
+  const {
+    user: {
+      contributionsCollection: {
+        contributionCalendar: { totalContributions },
+      },
+    },
+  } = await graphQLClient.request(query, variables);
+
+  console.log(totalContributions);
+
+  bot.sendMessage(1740567815, totalContributions);
+});
